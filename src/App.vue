5d08aff227fc3d57f527325793f4dca0f0f3445e
IamSleepy_zzZ <1093930651@qq.com>
2018-05-17 15:56:34 +0800

    wordcloud
    wordcloud

--- src/App.vue
+++ src/App.vue
@@ -13,45 +13,45 @@
 </template>
 
 <script>
-import * as d3 from 'd3v4'
+/*  import * as d3 from 'd3v4'*/
 
-import timeline from './components/timeline'
-import my_main from './components/main'
-import Inf from './components/Inf'
+  import timeline from './components/timeline'
+  import my_main from './components/main'
+  import Inf from './components/Inf'
 
-export default {
-  name: 'App',
-  data(){
-    return {
-      node:{},
-      my_timerange:[],
-      timerange:[{"date":"2017/11/02","mounts":9000},{"date":"2017/11/03","mounts":98},{"date":"2017/11/04","mounts":28},{"date":"2017/11/05","mounts":80},{"date":"2017/11/08","mounts":207},{"date":"2017/11/09","mounts":24},{"date":"2017/11/10","mounts":35},{"date":"2017/11/11","mounts":51},{"date":"2017/11/12","mounts":239},{"date":"2017/11/13","mounts":130},{"date":"2017/11/14","mounts":449},{"date":"2017/11/15","mounts":657},{"date":"2017/11/16","mounts":171},{"date":"2017/11/17","mounts":157},{"date":"2017/11/18","mounts":718},{"date":"2017/11/19","mounts":149},{"date":"2017/11/20","mounts":60},{"date":"2017/11/21","mounts":65},{"date":"2017/11/22","mounts":4},{"date":"2017/11/23","mounts":92},{"date":"2017/11/25","mounts":20},{"date":"2017/11/26","mounts":64},{"date":"2017/11/27","mounts":69},{"date":"2017/11/28","mounts":78},{"date":"2017/11/29","mounts":70},{"date":"2017/11/30","mounts":4000},{"date":"2017/12/01","mounts":34},{"date":"2017/12/02","mounts":29},{"date":"2017/12/03","mounts":353},{"date":"2017/12/04","mounts":236},{"date":"2017/12/05","mounts":244},{"date":"2017/12/06","mounts":135},{"date":"2017/12/07","mounts":356},{"date":"2017/12/08","mounts":327},{"date":"2017/12/09","mounts":231},{"date":"2017/12/10","mounts":224},{"date":"2017/12/11","mounts":37},{"date":"2017/12/12","mounts":268},{"date":"2017/12/13","mounts":5},{"date":"2017/12/18","mounts":389}]
-    }
-  },
-  components:{
-    timeline,
-    "my_main":my_main,
-    Inf
-  },
-  methods:{
-    timechange(range){
-      this.my_timerange=range
-      console.log(range)
+  export default {
+    name: 'App',
+    data(){
+      return {
+        node:{},
+        my_timerange:[],
+        timerange:[{"date":"2017/11/02","mounts":9000},{"date":"2017/11/03","mounts":98},{"date":"2017/11/04","mounts":28},{"date":"2017/11/05","mounts":80},{"date":"2017/11/08","mounts":207},{"date":"2017/11/09","mounts":24},{"date":"2017/11/10","mounts":35},{"date":"2017/11/11","mounts":51},{"date":"2017/11/12","mounts":239},{"date":"2017/11/13","mounts":130},{"date":"2017/11/14","mounts":449},{"date":"2017/11/15","mounts":657},{"date":"2017/11/16","mounts":171},{"date":"2017/11/17","mounts":157},{"date":"2017/11/18","mounts":718},{"date":"2017/11/19","mounts":149},{"date":"2017/11/20","mounts":60},{"date":"2017/11/21","mounts":65},{"date":"2017/11/22","mounts":4},{"date":"2017/11/23","mounts":92},{"date":"2017/11/25","mounts":20},{"date":"2017/11/26","mounts":64},{"date":"2017/11/27","mounts":69},{"date":"2017/11/28","mounts":78},{"date":"2017/11/29","mounts":70},{"date":"2017/11/30","mounts":4000},{"date":"2017/12/01","mounts":34},{"date":"2017/12/02","mounts":29},{"date":"2017/12/03","mounts":353},{"date":"2017/12/04","mounts":236},{"date":"2017/12/05","mounts":244},{"date":"2017/12/06","mounts":135},{"date":"2017/12/07","mounts":356},{"date":"2017/12/08","mounts":327},{"date":"2017/12/09","mounts":231},{"date":"2017/12/10","mounts":224},{"date":"2017/12/11","mounts":37},{"date":"2017/12/12","mounts":268},{"date":"2017/12/13","mounts":5},{"date":"2017/12/18","mounts":389}]
+      }
     },
-    nodeselect(node){
-      this.node=node
-      console.log(node)
-    }
-  },
-  computed:{
-    getnode(){
-      return this.node
+    components:{
+      timeline,
+      "my_main":my_main,
+      Inf
     },
-    gettimerange(){
-      return this.my_timerange
-    }
-  },
-  beforeCreate(){
+    methods:{
+      timechange(range){
+        this.my_timerange=range
+        console.log(range)
+      },
+      nodeselect(node){
+        this.node=node
+        console.log(node)
+      }
+    },
+    computed:{
+      getnode(){
+        return this.node
+      },
+      gettimerange(){
+        return this.my_timerange
+      }
+    },
+    beforeCreate(){
 /*    document.body.style.cursor="wait";
       this.$ajax.get('/api/getArticles')
       .then((response)=>{
@@ -61,8 +61,8 @@ export default {
       .catch(function (error) {
         console.log(error);
       });*/
+    }
   }
-}
 </script>
 
 <style>
@@ -75,9 +75,9 @@ body{
 }
 #app{
 /*  width: 1688px;
-  height: 1000px;*/
-  width: 99%;
-  height: 800px;
+height: 1000px;*/
+width: 99%;
+height: 800px;
 }
 #main_view{
   box-sizing: border-box;
--- src/components/main.vue
+++ src/components/main.vue
@@ -2,9 +2,9 @@
     <div id="graph" style="height:100%;width:100%"></div>    
 </template>
 
+<script type="text/javascript" src="../assets/d3.layout.cloud.js"></script>
 <script>
-import * as d3 from 'd3v4'
-
+/*import * as d3 from 'd3v4'*/
 export default {
   name: 'my_main',
   data () {
@@ -31,10 +31,7 @@ export default {
     var my_svg = d3.select("#graph").append("svg");
     var svg=my_svg.append("g")
 
-    var chartLayer = svg.append("g")
-                    .classed("chartLayer", true)
 
-    
     my_main(data)
     
     function my_main(x) {
@@ -44,7 +41,6 @@ export default {
         count++;
         nodes.push({"index":node.label,"level":node.level,"name":node.name,"r":node.r,"subjects_in":node.subjects_in,"subjects_out":node.subjects_out,"type":node.type,});
 
-/*        nodes.push({"label":"l"+node.label,"r":node.r});*/
         if (node.children!=undefined){
             node.children.forEach(function(item){
                 search(item);
@@ -80,14 +76,7 @@ export default {
         chartWidth = width - (margin.left+margin.right)
         chartHeight = height - (margin.top+margin.bottom)
         
-        my_svg.attr("width", width).attr("height", height)
-        
-        
-        chartLayer
-            .attr("width", chartWidth)
-            .attr("height", chartHeight)
-            .attr("transform", "translate("+[margin.left, margin.top]+")")
-            
+        my_svg.attr("width", width).attr("height", height)   
             
     }
     
@@ -126,20 +115,15 @@ export default {
         color["finance"]="#FAA24B";
         color["HR"]="green";
 
-        var node = svg.append("g")
+        var node_g = svg.append("g")
             .attr("class", "nodes")
-            .selectAll("circle")
+            .selectAll("g")
             .data(data.nodes)
-            .enter().append("circle")
-            .attr("class",function(d){  return d.type; })
-            .attr("r", function(d){  return d.r })
-            .attr("fill",function(d){return color[d.type]})
-            .attr("stroke",function(d){
-                 if(d.level==2)
-                    return "white"
-                else if (d.level==3 && d.type.indexOf("development")!=-1)
-                    return "#ffff00"
-            })
+            .enter()
+            .append('g')
+            .attr("class", function(d){
+                return "g_"+d.type
+            })            
             .style("cursor","hand")
             .on("mousedown",function(d){
                 that.selectnode(d);
@@ -154,26 +138,70 @@ export default {
             .call(d3.drag()
                 .on("start", dragstarted)
                 .on("drag", dragged)
-                .on("end", dragended));    
-        
-        
+                .on("end", dragended))
+
+         var node=node_g.append("circle")
+            .attr("class",function(d){  return d.type; })
+            .attr("r", function(d){  return d.r })
+            .attr("fill",function(d){return color[d.type]})
+            .attr("stroke",function(d){
+                 if(d.level==2)
+                    return "white"
+                else if (d.level==3 && d.type.indexOf("development")!=-1)
+                    return "#ffff00"
+            })
+
+             //word-cloud--------------------------
+
+                 var frequency_list = [{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10},{"text":"electricity","size":15},{"text":"movement","size":10},{"text":"relation","size":5},{"text":"things","size":10},{"text":"force","size":5},{"text":"ad","size":5},{"text":"energy","size":85},{"text":"living","size":5},{"text":"nonliving","size":5},{"text":"laws","size":15},{"text":"speed","size":45},{"text":"velocity","size":30},{"text":"define","size":5},{"text":"constraints","size":5},{"text":"universe","size":10},{"text":"physics","size":120},{"text":"describing","size":5},{"text":"matter","size":90},{"text":"physics-the","size":5},{"text":"world","size":10},{"text":"works","size":10},{"text":"science","size":70},{"text":"interactions","size":30},{"text":"studies","size":5},{"text":"properties","size":45},{"text":"nature","size":40},{"text":"branch","size":30},{"text":"concerned","size":25},{"text":"source","size":40},{"text":"google","size":10},{"text":"defintions","size":5},{"text":"two","size":15},{"text":"grouped","size":15},{"text":"traditional","size":15},{"text":"fields","size":15},{"text":"acoustics","size":15},{"text":"optics","size":15},{"text":"mechanics","size":20},{"text":"thermodynamics","size":15},{"text":"electromagnetism","size":15},{"text":"modern","size":15},{"text":"extensions","size":15},{"text":"thefreedictionary","size":15},{"text":"interaction","size":15},{"text":"org","size":25},{"text":"answers","size":5},{"text":"natural","size":15},{"text":"objects","size":5},{"text":"treats","size":10},{"text":"acting","size":5},{"text":"department","size":5},{"text":"gravitation","size":5},{"text":"heat","size":10},{"text":"light","size":10},{"text":"magnetism","size":10},{"text":"modify","size":5},{"text":"general","size":10},{"text":"bodies","size":5},{"text":"philosophy","size":5},{"text":"brainyquote","size":5},{"text":"words","size":5},{"text":"ph","size":5},{"text":"html","size":5},{"text":"lrl","size":5},{"text":"zgzmeylfwuy","size":5},{"text":"subject","size":5},{"text":"distinguished","size":5},{"text":"chemistry","size":5},{"text":"biology","size":5},{"text":"includes","size":5},{"text":"radiation","size":5},{"text":"sound","size":5},{"text":"structure","size":5},{"text":"atoms","size":5},{"text":"including","size":10},{"text":"atomic","size":10},{"text":"nuclear","size":10},{"text":"cryogenics","size":10},{"text":"solid-state","size":10},{"text":"particle","size":10},{"text":"plasma","size":10},{"text":"deals","size":5},{"text":"merriam-webster","size":5},{"text":"dictionary","size":10},{"text":"analysis","size":5},{"text":"conducted","size":5},{"text":"order","size":5},{"text":"understand","size":5},{"text":"behaves","size":5},{"text":"en","size":5},{"text":"wikipedia","size":5},{"text":"wiki","size":5},{"text":"physics-","size":5},{"text":"physical","size":5},{"text":"behaviour","size":5},{"text":"collinsdictionary","size":5},{"text":"english","size":5},{"text":"time","size":35},{"text":"distance","size":35},{"text":"wheels","size":5},{"text":"revelations","size":5},{"text":"minute","size":5},{"text":"acceleration","size":20},{"text":"torque","size":5},{"text":"wheel","size":5},{"text":"rotations","size":5},{"text":"resistance","size":5},{"text":"momentum","size":5},{"text":"measure","size":10},{"text":"direction","size":10},{"text":"car","size":5},{"text":"add","size":5},{"text":"traveled","size":5},{"text":"weight","size":5},{"text":"electrical","size":5},{"text":"power","size":5}];
+
+
+                
+                    var color = d3.scaleLinear()
+                            .domain([0,1,2,3,4,5,6,10,15,20,100])
+                            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+
+
+                        d3.layout.cloud().size([800, 300])
+                            .words(frequency_list)
+                            .rotate(0)
+                            .fontSize(function(d) { return d.size; })
+                            .on("end", draw)
+                            .start();
+
+                    function draw(words) {
+                        d3.select(".g_Boos")
+                                // without the transform, words words would get cutoff to the left and top, they would
+                                // appear outside of the SVG area
+                                .attr("transform", "translate(320,200)")
+                                .selectAll("text")
+                                .data(words)
+                                .enter().append("text")
+                                .style("font-size", function(d) { return d.size + "px"; })
+                                .style("fill", function(d, i) { return color(i); })
+                                .attr("transform", function(d) {
+                                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                                })
+                                .text(function(d) { return d.text; });
+                    }
+
         var ticked = function() {
             link
                 .attr("x1", function(d) { return d.source.x; })
                 .attr("y1", function(d) { return d.source.y; })
                 .attr("x2", function(d) { return d.target.x; })
                 .attr("y2", function(d) { return d.target.y; });
+
+            node_g.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"});
     
-            node
-                .attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
         }  
         simulation
             .nodes(data.nodes)
             .on("tick", ticked);
     
         simulation.force("link")
-            .links(data.links);    
+            .links(data.links);   
+
 
         var zoom_handler = d3.zoom()
         .on("zoom", zoom_actions);
